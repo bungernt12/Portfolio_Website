ef35e8b11a1a220acbfc485988dc8a39c6743534
Tague Zachary <taguezz@gmail.com>
2024-01-02 12:51:55 -0800

    format z-index js for image container

--- portfolio-main.js
+++ portfolio-main.js
@@ -1,5 +1,25 @@
-const shrinkClick = event => {
-    event.target.style.width  = '20%';
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.hoverable-image');
 
-document.addEventListener('click', shrinkClick);
+    images.forEach(image => {
+        image.addEventListener('mouseenter', function () {
+            // Grow the hovered image
+            this.style.transform = 'scale(1.2)';
+            this.style.zIndex = '2'
+            //z-index didn't work to make roshi above 
+            
+            // Shrink the other images
+            images.forEach(otherImage => {
+                if (otherImage !== this) {
+                    otherImage.style.transform = 'scale(1)';
+                    otherImage.style.zIndex = '1';
+                }
+            });
+        });
+
+        image.addEventListener('mouseleave', function () {
+            // Shrink the image back to its original size on mouse leave
+            this.style.transform = 'scale(1)';
+        });
+    });
+});
